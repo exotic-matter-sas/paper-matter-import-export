c48747b0c3212e5ced87517891419ad612b2172e
fabien <fabien@exotic-matter.fr>
2019-10-23 08:59:44 +0200

    Initial electron-vue setup

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'paper-matter-import-export'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
