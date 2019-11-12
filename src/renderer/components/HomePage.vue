47fead0a56a2fe8934734eaf93626823204f55d9
fabien <fabien@exotic-matter.fr>
2019-11-12 12:24:20 +0100

    Include logged header inside HomePage tabs in order it appears at the right of tabs, fine tune css

--- src/renderer/components/HomePage.vue
+++ src/renderer/components/HomePage.vue
@@ -1,12 +1,17 @@
 <template>
   <b-container fluid class="min-vh-100 d-flex flex-column">
-    <LoggedHeader/>
-    <ul class="nav nav-tabs row bg-dark" role="tablist">
+    <ul class="nav nav-tabs row bg-dark align-items-center" role="tablist">
       <li class="nav-item col">
         <a class="nav-link active text-center" id="import-tab" data-toggle="tab" href="#import" role="tab" aria-controls="home" aria-selected="true">Import</a>
       </li>
       <li class="nav-item col">
         <a class="nav-link text-center" id="export-tab" data-toggle="tab" href="#export" role="tab" aria-controls="profile" aria-selected="false">Export</a>
+      </li>
+      <li id="logged-header" class="col  text-white-50">
+        <button @click.prevent="disconnectUser" type="button" aria-label="Disconnect" class="close text-white-50" title="Disconnect">
+          ×
+        </button>
+        Logged as: <span :title="accountName">{{accountName}}</span>
       </li>
     </ul>
     <div class="tab-content row flex-grow-1">
@@ -27,17 +32,16 @@
 </template>
 
 <script>
-  import LoggedHeader from "./HomePage/LoggedHeader";
   import ImportTab from "./HomePage/ImportTab";
   import ExportTab from "./HomePage/ExportTab";
   import ProgressModal from "./HomePage/ProgressModal";
+  import {mapState} from "vuex";
 
   const log = require('electron-log');
 
   export default {
     name: 'home',
     components: {
-        LoggedHeader,
         ImportTab,
         ExportTab,
         ProgressModal
@@ -52,7 +56,15 @@
       }
     },
 
+    computed: {
+      ...mapState('auth', ['accountName'])
+    },
+
     methods: {
+      disconnectUser () {
+        this.$store.dispatch('auth/disconnectUser', 'user disconnect himself');
+      },
+
       displayImportProgress(totalCount) {
           this.actionOnGoing = true;
           this.actionType = 'import';
@@ -75,11 +87,25 @@
 <style lang="scss" scoped>
   @import '../customBootstrap.scss';
 
+  ul.nav {
+    flex-wrap: wrap-reverse;
+    li {
+      margin-top: 15px;
+      padding:0 0 0 15px;
+
+      &:last-child{
+        padding-right: 15px;
+      }
+    }
+  }
+
   .nav-tabs .nav-link {
     color: $white-50;
     border: 1px solid $white-50;
     border-bottom: 0;
     font-weight: bold;
+    padding: 0.75em 0 0.75em 0;
+
     &:hover{
       color: $white-75;
       border: 1px solid $white-75;
@@ -89,5 +115,28 @@
       color: map_get($theme-colors, 'primary');
       background: $light;
     }
+  }
+
+  #logged-header{
+    font-size: 0.9em;
+
+    span {
+      font-weight: bold;
+      color: $white-75;
+      display: inline-block;
+      max-width: 100%;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      vertical-align: middle;
+    }
+
+    button.close{
+      font-size: 1.5em;
+      vertical-align: -0.1em;
+    }
+  }
+
+  .close:hover{
+    color: $white-75;
   }
 </style>
