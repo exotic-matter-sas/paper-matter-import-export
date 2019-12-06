8c9101fa56aa77fb3773f5081e3f8149ecef041c
Antoine L <antoine@exotic-matter.fr>
2019-12-06 13:59:32 +0100

    Enable auto update

--- src/main/index.js
+++ src/main/index.js
@@ -68,7 +68,6 @@ app.on('activate', () => {
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
  */
 
-/*
 import { autoUpdater } from 'electron-updater'
 
 autoUpdater.on('update-downloaded', () => {
@@ -78,4 +77,4 @@ autoUpdater.on('update-downloaded', () => {
 app.on('ready', () => {
   if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
 })
- */
+
