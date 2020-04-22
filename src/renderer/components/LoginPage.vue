exotic-matter-sas/paper-matter-import-export

commit 5d2579db1d5aac024b80d6cf6ad66e0031d52d54

Remove complex logic for resizing window to web content height to a simple resize with an hardcoded pixel height value for each view, fine tune `LoginPage.vue` css to center vertically elements in order page look great with and without the basic login error message.

src/renderer/components/LoginPage.vue
@@ -5,38 +5,42 @@
 
 <template>
   <b-container>
-    <b-row class="align-items-center">
+    <b-row class="align-items-center min-vh-100">
       <b-col>
-        <form v-if="!refreshPending" id="login-form">
-          <img src="~@/assets/colors_logo.svg" :alt="$t('loginPage.logoAlt')" class="d-block mx-auto my-3">
-          <div class="form-label-group">
-            <input autofocus="" class="form-control" id="id_email" name="username"
-                   :placeholder="$t('loginPage.emailInputLabel')" required="" type="text" v-model="email">
-            <label for="id_email">{{ $t('loginPage.emailInputLabel')}}</label>
-          </div>
-          <div class="form-label-group">
-            <input class="form-control" id="id_password" name="password"
-                   :placeholder="$t('loginPage.passwordInputLabel' )" required=""
-                   type="password"  v-model="password">
-            <label for="id_password">{{ $t('loginPage.passwordInputLabel')}}</label>
-            <a class="mt-1 d-block" href @click.prevent="open('https://papermatter.app/password_reset/')"
-                    id="password-reset">{{ $t('loginPage.forgotPasswordLink')}}
-            </a>
-          </div>
-          <div v-if="lastError" class="alert alert-danger">{{lastError}}</div>
-          <input class="btn btn-lg btn-primary btn-block mb-3" type="submit" :value="$t('loginPage.submitInputValue')"
-                 @click.prevent="login" :disabled="loginPending || !(login && password)">
-        </form>
+        <b-row>
+          <b-col>
+            <form v-if="!refreshPending" id="login-form">
+              <img src="~@/assets/colors_logo.svg" :alt="$t('loginPage.logoAlt')" class="d-block mx-auto my-3">
+              <div class="form-label-group">
+                <input autofocus="" class="form-control" id="id_email" name="username"
+                       :placeholder="$t('loginPage.emailInputLabel')" required="" type="text" v-model="email">
+                <label for="id_email">{{ $t('loginPage.emailInputLabel')}}</label>
+              </div>
+              <div class="form-label-group">
+                <input class="form-control" id="id_password" name="password"
+                       :placeholder="$t('loginPage.passwordInputLabel' )" required=""
+                       type="password"  v-model="password">
+                <label for="id_password">{{ $t('loginPage.passwordInputLabel')}}</label>
+                <a class="mt-1 d-block" href @click.prevent="open('https://papermatter.app/password_reset/')"
+                   id="password-reset">{{ $t('loginPage.forgotPasswordLink')}}
+                </a>
+              </div>
+              <div v-if="lastError" class="alert alert-danger">{{lastError}}</div>
+              <input class="btn btn-lg btn-primary btn-block mb-3" type="submit" :value="$t('loginPage.submitInputValue')"
+                     @click.prevent="login" :disabled="loginPending || !(login && password)">
+            </form>
 
-        <div v-else class="text-center">
-          <b-spinner type="grow" variant="primary" :label="$t('loginPage.loadingSpinnerLabel')"
-                     style="width:4em;height:4em;"></b-spinner>
-        </div>
-      </b-col>
-    </b-row>
-    <b-row id="domain-footer" class="mb-3">
-      <b-col class="text-center">
-          <label class="d-inline">{{ $t('loginPage.loginDomainLabel') }}<b>{{this.apiBaseUrl}}</b></label>
+            <div v-else class="text-center">
+              <b-spinner type="grow" variant="primary" :label="$t('loginPage.loadingSpinnerLabel')"
+                         style="width:4em;height:4em;"></b-spinner>
+            </div>
+          </b-col>
+        </b-row>
+        <b-row id="domain-footer">
+          <b-col class="text-center">
+            <label class="d-inline">{{ $t('loginPage.loginDomainLabel') }}<b>{{this.apiBaseUrl}}</b></label>
+          </b-col>
+        </b-row>
       </b-col>
     </b-row>
   </b-container>
@@ -44,7 +48,7 @@
 
 
 <script>
-    import {ipcRenderer} from "electron";
+    import {ipcRenderer, remote} from "electron";
     import {mapState} from "vuex";
     const log = require('electron-log');
 
@@ -52,20 +56,20 @@
         name: 'login',
 
         data(){
-            return{
-                email: '',
-                password: '',
-                loginPending: false,
-                refreshPending: false,
-                lastError: ''
-            }
+          return{
+            windowHeight: 438,
+            email: '',
+            password: '',
+            loginPending: false,
+            refreshPending: false,
+            lastError: ''
+          }
         },
 
         mounted () {
-            // to resize window to content
-            this.$nextTick().then(() => ipcRenderer.send('vue-did-finish-load'));
-            // redirect to home if user access token is set (and still valid or can be refresh)
-            this.skipLoginIfAuthenticated();
+          // to resize window to page content
+          const window = remote.getCurrentWindow();
+          window.setContentSize(window.getContentSize()[0], this.windowHeight); // keep same width
         },
 
         computed: {
@@ -115,8 +119,6 @@
                         } else if (error.request) {
                          vi.lastError = 'The Paper Matter server seems unreachable, please check your connection.'
                         }
-                        // to resize window to content when error message appears
-                        this.$nextTick().then(() => ipcRenderer.send('vue-did-finish-load'));
                     });
 
                 vi.loginPending = false;

src/main/index.js
@@ -18,20 +18,6 @@ const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`;
 
-function fitWindowHeightToContent (window) {
-    window.webContents.executeJavaScript(
-      'function getContentHeight() {' +
-        'if (document.readyState === "complete"){' +
-          'return document.querySelector(\'.container,.container-fluid\').scrollHeight;' +
-        '} else {' +
-          'setTimeout(getContentHeight, 500);' +
-        '}' +
-      '};getContentHeight();'
-    ).then((contentHeight) => {
-      window.setContentSize(window.getContentSize()[0], contentHeight); // keep same width
-    });
-}
-
 function createWindow () {
   /**
    * Initial window options
@@ -53,11 +39,6 @@ function createWindow () {
   mainWindow.setAutoHideMenuBar(true);
 
   mainWindow.loadURL(winURL);
-
-  // On Vue load
-  ipcMain.on('vue-did-finish-load', (event, message) => {
-    fitWindowHeightToContent(mainWindow);
-  });
 
   mainWindow.on('close', (event) => {
     event.preventDefault();

src/renderer/components/HomePage.vue
@@ -45,7 +45,7 @@
 </template>
 
 <script>
-  import {ipcRenderer} from "electron";
+  import {remote} from "electron";
   import ImportTab from "./HomePage/ImportTab";
   import ExportTab from "./HomePage/ExportTab";
   import ProgressModal from "./HomePage/ProgressModal";
@@ -65,17 +65,19 @@
 
     data() {
       return {
-          actionType: 'import', // TODO make this value dynamic based on active tab
-          actionOnGoing: false,
-          totalCount: 0,
-          importInterrupted: false,
-          pickingFolder: false
+        windowHeight: 386,
+        actionType: 'import', // TODO make this value dynamic based on active tab
+        actionOnGoing: false,
+        totalCount: 0,
+        importInterrupted: false,
+        pickingFolder: false
       }
     },
 
     mounted() {
-      // to resize window to content
-      this.$nextTick().then(() => ipcRenderer.send('vue-did-finish-load'));
+      // to resize window to page content
+      const window = remote.getCurrentWindow();
+      window.setContentSize(window.getContentSize()[0], this.windowHeight); // keep same width
     },
 
     computed: {

commit 3b940d47041f4511dbd46e3e1c05158884b2926d

Fix a regression introduce by `5d2579db1d5aac024b80d6cf6ad66e0031d52d54`, skipLoginIfAuthenticated not called (i.e. the auto login / remember me feature wasn't working)

src/renderer/components/LoginPage.vue
@@ -52,7 +52,7 @@
 
 
 <script>
-    import {ipcRenderer, remote} from "electron";
+    import {remote} from "electron";
     import {mapState} from "vuex";
     import EditServerAddressModal from "./LoginPage/EditServerAddressModal";
     import {defaultApiHostName} from "./../store/modules/config";
@@ -77,6 +77,8 @@
           // to resize window to page content
           const window = remote.getCurrentWindow();
           window.setContentSize(window.getContentSize()[0], this.windowHeight); // keep same width
+          // redirect to home if user access token is set (and still valid or can be refresh)
+          this.skipLoginIfAuthenticated();
         },
 
         computed: {