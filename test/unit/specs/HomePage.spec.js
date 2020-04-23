6cc0532498c8935214e558166659e34ff468fc86
fabien <fabien@exotic-matter.fr>
2020-04-23 11:17:34 +0200

    Add `HomePage.spec.js`

---
+++ test/unit/specs/HomePage.spec.js
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2020 Exotic Matter SAS. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ */
+
+import { createLocalVue, shallowMount } from "@vue/test-utils";
+import sm from "simple-mock"
+
+import BootstrapVue from "bootstrap-vue";
+import Vuex from "vuex";
+import storeConfig from "../../../src/renderer/store";
+import cloneDeep from "lodash.clonedeep";
+
+
+import HomePage from "../../../src/renderer/components/HomePage";
+
+import {remote} from "electron";
+import {USER_PROPS} from "../../tools/testValues";
+
+// Create clean Vue instance and set installed package to avoid warning
+const localVue = createLocalVue();
+
+// Mock BootstrapVue prototypes here (eg. localVue.prototype.$bvModal = {msgBoxConfirm: jest.fn()}; )
+localVue.use(BootstrapVue); // avoid bootstrap vue warnings
+localVue.use(Vuex);
+localVue.component("font-awesome-icon", ); // avoid font awesome warnings
+
+
+// Mock prototype and mixin bellow
+localVue.prototype.$t = (text, args = "") => {
+  return text + args;
+}; // i18n mock
+localVue.prototype.$tc = (text, args = "") => {
+  return text + args;
+}; // i18n mock
+
+describe("HomePage template", () => {
+  let wrapper;
+  let storeConfigCopy;
+  let store;
+
+  beforeEach(() => {
+    storeConfigCopy = cloneDeep(storeConfig);
+    store = new Vuex.Store(storeConfigCopy);
+    const accountNameMock = sm.mock().returnWith(USER_PROPS.email);
+
+    wrapper = shallowMount(HomePage, {
+      localVue,
+      store,
+      computed: {
+        accountName: accountNameMock
+      }
+    });
+  });
+
+  afterEach(() => {
+    sm.restore();
+  });
+
+  it("renders properly html element", () => {
+    const elementSelector = "#logged-header";
+    const elem = wrapper.find(elementSelector);
+    expect(elem.is(elementSelector)).to.equal(true);
+  });
+
+  it("renders properly component data", async () => {
+    expect(wrapper.text()).to.contains(USER_PROPS.email);
+  });
+});
+
+describe("HomePage mounted", () => {
+  let wrapper;
+  let store;
+  let storeConfigCopy;
+
+  let fakeWidth;
+  let setContentSizeMock;
+  let getContentSizeMock;
+  let getCurrentWindowMock;
+
+  beforeEach(() => {
+    fakeWidth = 100;
+    setContentSizeMock = sm.mock();
+    getContentSizeMock = sm.mock().returnWith([fakeWidth, 0]);
+    getCurrentWindowMock = sm.mock(remote, "getCurrentWindow").returnWith(
+      {setContentSize: setContentSizeMock, getContentSize: getContentSizeMock}
+    );
+    storeConfigCopy = cloneDeep(storeConfig);
+    store = new Vuex.Store(storeConfigCopy);
+    wrapper = shallowMount(HomePage, {
+      localVue,
+      store,
+    });
+  });
+
+  afterEach(() => {
+    sm.restore();
+  });
+
+  it("electron remote.setContentSize is called to set window size", () => {
+    console.log(getCurrentWindowMock);
+    expect(getCurrentWindowMock.callCount).to.equal(1);
+    expect(setContentSizeMock.callCount).to.equal(1);
+    expect(setContentSizeMock.lastCall.args[0]).to.equal(fakeWidth); // come from getContentSize return, first item
+    expect(setContentSizeMock.lastCall.args[1]).to.equal(wrapper.vm.windowHeight); // come from windowHeight data
+  });
+});
+
+describe("HomePage methods", () => {
+  let wrapper;
+  let storeConfigCopy;
+  let store;
+  let disconnectUserMock;
+
+  beforeEach(() => {
+    disconnectUserMock = sm.mock();
+    storeConfigCopy = cloneDeep(storeConfig);
+    storeConfigCopy.modules.auth.actions.disconnectUser = disconnectUserMock;
+    store = new Vuex.Store(storeConfigCopy);
+    wrapper = shallowMount(HomePage, {
+      localVue,
+      store,
+    });
+  });
+
+  afterEach(() => {
+    sm.restore();
+  });
+
+  it("disconnectUser call disconnectUser action", () => {
+    wrapper.vm.disconnectUser();
+
+    expect(disconnectUserMock.callCount).to.equal(1);
+  });
+
+  it("displayImportProgress set proper values", () => {
+    const fakeTotalCount = 42;
+    wrapper.vm.displayImportProgress(fakeTotalCount);
+
+    expect(wrapper.vm.actionOnGoing).to.equal(true);
+    expect(wrapper.vm.totalCount).to.equal(fakeTotalCount);
+  });
+
+  it("interruptImport call set importInterrupted to true", () => {
+    wrapper.vm.interruptImport();
+
+    expect(wrapper.vm.importInterrupted).to.equal(true);
+  });
+
+  it("hideImportProgress set proper values", () => {
+    wrapper.vm.hideImportProgress();
+
+    expect(wrapper.vm.actionOnGoing).to.equal(false);
+    expect(wrapper.vm.totalCount).to.equal(0);
+    expect(wrapper.vm.importInterrupted).to.equal(false);
+  });
+});
